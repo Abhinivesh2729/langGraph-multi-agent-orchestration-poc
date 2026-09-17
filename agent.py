c332a8edb781e2b0cf94480f84761524eb72c432
Abhinivesh2729 <abhinivesh.smail@gmail.com>
2026-09-17 08:38:14 +0530

    added multi node

--- agent.py
+++ agent.py
@@ -1,21 +1,37 @@
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 
+#Define State
 class State(TypedDict):
     a: int
     b: int
     result: int
+    operation: str
+    message: str
 
-def add_nums(state: State) -> State:
-    return {"result":state["a"] + state["b"]}
+# Nodes
+def add_nums(state: State):
+    return {"result":state["a"] + state["b"], "operation": "Addition"}
 
+def verbose(state: State):
+    return {"message": f"The result for {state["operation"]} is {state["result"]}"}
+
+def print_result(state: State):
+    print(state["message"])
+
+#Edges
 graph = StateGraph(State)
 
 graph.add_node("add",add_nums)
+graph.add_node("verbose", verbose)
+graph.add_node("print", print_result)
+
 graph.add_edge(START, "add")
-graph.add_edge("add", END)
+graph.add_edge("add", "verbose")
+graph.add_edge("verbose","print")
+graph.add_edge("print", END)
+
 
 agent = graph.compile()
 
-result = agent.invoke({'a':10,'b':20})
-print(result["result"])
+result = agent.invoke({'a':10,'b':17})
