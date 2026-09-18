a3f06a7118fb314160a943047a17d08b2bc91a6d
Abhinivesh2729 <abhinivesh.smail@gmail.com>
2026-09-18 08:49:06 +0530

    check pointer added

--- README.md
+++ README.md
@@ -8,4 +8,33 @@
 
 
 
+### Chat: Checkpointer + Thread-Id
 
+**User 1:** Hello, AI. I'm Mr. Peter Parker.
+
+**AI:** Hello, Mr. Parker! How can I assist you today?
+
+**User 1:** Can you get my age?
+
+**AI:** According to my records, your age is 27.
+
+**User 1:** Good, I have my grandpa. His ID is 29. Say his age.
+
+**AI:** According to my records, your grandpa's age is 81.
+
+### Chat: User 29
+
+**User 29:** Hello, my name is Dall.
+
+**AI:** Hello, Dall. How can I assist you today?
+
+**User 29:** Can you get my age and my grandson's age? His ID is 1.
+
+**AI:** Sure, here are the ages you asked for:
+
+- Your age is 81.
+- Your grandson's age is 27.
+
+**User 29:** Do you know my grandson's name?
+
+**AI:** Based on the information provided, your grandson's name is not available. We only have his age, which is 27. If you have any additional details or if there's a way to retrieve his name, please let me know!
---
+++ agent_with_memory.py
@@ -0,0 +1,69 @@
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.prebuilt.tool_node import ToolNode, tools_condition
+from langchain_ollama import ChatOllama
+import sqlite3
+
+#state
+class State(MessagesState):
+    userId: int
+
+#tools
+@tool
+def get_age(userId: int) -> str:
+    """While passing the user id as int this function returns the age of the user as string """
+    if(userId > 5):
+        return f"The age of the user-{userId} is 81"
+    else:
+        return f"The user-{userId} age is 27"
+
+#LLM
+llm = ChatOllama(model="qwen2.5:3b",temperature=0.3)
+llm_with_tools = llm.bind_tools([get_age])
+
+#Memory
+connection = sqlite3.connect("agent.db",check_same_thread=False)
+memory = SqliteSaver(conn=connection)
+
+#Nodes
+def chat_node(state: State):
+    response = llm_with_tools.invoke([SystemMessage(f"You are a Ai Assistent, your name is Jeff, current userId is {state["userId"]}"),*state["messages"]])
+    return {"messages": [response]}
+
+def print_message(state: State):
+    print(f"AI: {state['messages'][-1].content}")
+
+tool_node = ToolNode([get_age])
+
+#Router
+def custom_router(state: State) -> str:
+    if(state["messages"][-1].tool_calls):
+        return "tools"
+    else:
+        return "print"
+    
+#Edge
+builder = StateGraph(State)
+builder.add_node("chat", chat_node)
+builder.add_node("print", print_message)
+builder.add_node("tools", tool_node)
+
+builder.add_edge(START, "chat")
+builder.add_conditional_edges("chat",custom_router,{"tools":"tools","print":"print"})
+builder.add_edge("tools","chat")
+builder.add_edge("print",END)
+
+#runtime
+graph = builder.compile(checkpointer=memory)
+userId = int(input("User ID: "))
+
+while True:
+    userInput = input(f"User-{userId}: ")
+    if(userInput == "exit"):
+        break
+    elif(userInput == "/user"):
+        userId = int(input("User ID: "))
+    else:
+        result = graph.invoke({"messages":[HumanMessage(content=userInput)], "userId": userId}, config={"configurable":{"thread_id": userId}})
